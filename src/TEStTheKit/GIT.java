4ed87d4589f84e1d5228b5f53992f2682eb9c714
sbretl <bretlsofia@gmail.com>
2021-03-12 23:46:08 -0500

    new file git added

---
+++ src/TEStTheKit/GIT.java
@@ -0,0 +1,11 @@
+package TEStTheKit;
+
+public class GIT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello sof");
+System.out.println("sof is learning git");
+	}
+
+}
